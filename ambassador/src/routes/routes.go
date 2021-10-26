5b9605b006361076dd0576fd03bf280f0d60e031
IGOR DE PAULA SILVA <dpsigor@gmail.com>
2021-10-26 01:24:46 -0300

    get user & logout

--- ambassador/src/routes/routes.go
+++ ambassador/src/routes/routes.go
@@ -10,6 +10,8 @@ func Setup(app *fiber.App) {
 	api := app.Group("api")
 
 	admin := api.Group("admin")
-	admin.Post("/register", controllers.Register)
-	admin.Post("/login", controllers.Login)
+	admin.Post("register", controllers.Register)
+	admin.Post("login", controllers.Login)
+	admin.Get("user", controllers.User)
+	admin.Get("logout", controllers.Logout)
 }
