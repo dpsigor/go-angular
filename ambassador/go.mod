ae4e6f1f910a63f4d590075513aac9d27e6c17fb
IGOR DE PAULA SILVA <dpsigor@gmail.com>
2021-11-13 18:31:17 -0300

    redis

--- ambassador/go.mod
+++ ambassador/go.mod
@@ -5,7 +5,10 @@ go 1.16
 require (
 	github.com/bxcodec/faker/v3 v3.6.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis/v8 v8.11.4
 	github.com/gofiber/fiber/v2 v2.20.2
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.16
--- ambassador/src/controllers/productController.go
+++ ambassador/src/controllers/productController.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -74,7 +77,6 @@ func UpdateProduct(c *fiber.Ctx) error {
 			"message": "product not found or without changes",
 		})
 	}
-
 	return c.JSON(product)
 }
 
@@ -94,6 +96,27 @@ func DeleteProduct(c *fiber.Ctx) error {
 			"message": "product not found",
 		})
 	}
-
 	return nil
+}
+
+func ProductsFrontend(c *fiber.Ctx) error {
+	var products []models.Product
+	var ctx = context.Background()
+	result, err := database.Cache.Get(ctx, "products_frontend").Result()
+	if err != nil {
+		database.DB.Find(&products)
+		bytes, err := json.Marshal(products)
+		if err != nil {
+			panic(err)
+		}
+		if err = database.Cache.Set(ctx, "products_frontend", bytes, 30*time.Minute).Err(); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		err = json.Unmarshal([]byte(result), &products)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return c.JSON(products)
 }
