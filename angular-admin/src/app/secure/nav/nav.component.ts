7140321c5b2a7adf19e4c4ae7b2be78f27445e72
IGOR DE PAULA SILVA <dpsigor@gmail.com>
2021-11-14 19:18:49 -0300

    get user name on navbar

--- angular-admin/src/app/secure/nav/nav.component.ts
+++ angular-admin/src/app/secure/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  user: User | null = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+
+  }
 
   ngOnInit(): void {
+    this.authService.user().subscribe(
+      user => {
+        this.user = user;
+      }
+    )
   }
 
 }
--- angular-admin/src/app/services/auth.service.ts
+++ angular-admin/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,16 @@ export class AuthService {
     private http: HttpClient,
   ) { }
 
-  register(data: any): Observable<any> {
-    return this.http.post(`${environment.api}/register`, data);
+  register(data: any): Observable<User> {
+    return this.http.post<User>(`${environment.api}/register`, data);
   }
 
   login(data: any): Observable<any> {
     return this.http.post(`${environment.api}/login`, data, { withCredentials: true });
+  }
+
+  user(): Observable<User> {
+    return this.http.get<User>(`${environment.api}/user`, { withCredentials: true });
   }
 
 }
